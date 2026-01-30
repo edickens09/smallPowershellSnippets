dbac456101d6ccd03e5194139313f033f0d0ba21
Eric Dickens <eric@neovivify.com>
2026-01-30 09:57:24 -0500

    upgrading delete admin script

--- deleteSetupAdmin.ps1
+++ deleteSetupAdmin.ps1
@@ -1,22 +1,27 @@
-$User = "wbra.admin"
+$admin = ".admin"
+$dadamin = ".dadmin"
+$Users = Get-ChildItem C:\Users | Where-Object {$_.Name -like "*$admin*" or $_.Name -like "*$dadmin" }
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
         Start-Process powershell.exe "-NoProfile -File `"$PSCommandPath`"" -Verb RunAs
         Exit
 	}
-if (Get-LocalUser -Name $User -ErrorAction SilentlyContinue) {
+foreach ($User in $Users) {
+    if (Get-LocalUser -Name $User -ErrorAction SilentlyContinue) {
+        Remove-LocalUser -Name $User
+        Write-Host "User has been removed"
+    }else{
+        Write-Host "User not Present"
+    }
 
-    Remove-LocalUser -Name $User
-    Write-Host "User has been removed"
-}else{
-    Write-Host "User not Present"
+    $CimInstance = Get-CimInstance -Class Win32_UserProfile | Where-Object {$_.LocalPath.split('\')[-1] -eq $User}
+    if ($CimInstance) {
+        Remove-CimInstance $CimInstance
+        Write-Host "User Profile is removed"
+    }else {
+        Write-Host "No Profile to remove"
+    }
 }
 
-$CimInstance = Get-CimInstance -Class Win32_UserProfile | Where-Object {$_.LocalPath.split('\')[-1] -eq $User}
-if ($CimInstance) {
-    Remove-CimInstance $CimInstance
-    Write-Host "User Profile is removed"
-}else{
-    Write-Host "No Profile to remove"
-}
 Read-Host -Prompt "Press Enter to exit"
+
--- removeUserProfile.ps1
+++ removeUserProfile.ps1
@@ -1,19 +1,18 @@
 #Remove User Profile
 #Display all users, run in loop and take input delete user input
 #Exit on "Exit" and redisplay on 1
-Get-ChildItem C:\Users
-
 $Selection = 0
 
-While ($Selection -ne 4 -and -ne "exit") {
+While ($Selection -ne "4" -and $Selection -ne "exit") {
     Write-Output "1 Delete User"
     Write-Output "4 Exit"
     $Selection = Read-Host "Please Enter your selection: "
 
-    if ($Selection = 1) {
+    if ($Selection -eq "1") {
+        Get-ChildItem C:\Users
+
         $User = Read-Host "Enter user to delete"
         Get-CimInstance -Class Win32_UserProfile | Where-Object {$_.LocalPath.split('\')[-1] -eq $User } | Remove-CimInstance
 
-        Get-ChildItem C:\Users
     }
 }
